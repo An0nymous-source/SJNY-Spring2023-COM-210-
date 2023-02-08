ca363640895a6a32fc182b97af5e34e9d7a690ab
An0nymous-source <78245267+An0nymous-source@users.noreply.github.com>
2023-02-07 19:27:56 -0500

    Add files via upload

---
+++ ProductListAverage.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class ProductListAverage {
+
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+
+    String productNames = "";
+    Double productPrices = 0.0;
+
+    String finalOutput = "";
+
+    double averagePrice = 0;
+
+    double total = 0;
+
+    System.out.println("Enter the number of items in the list");
+    int itemNumber = sc.nextInt();
+
+    System.out.println("Enter the product name following by the price");
+
+    for (int i = 0; i < itemNumber; i++) {
+
+      productNames = sc.next();
+      productPrices = sc.nextDouble();
+
+      finalOutput += productNames + " " + productPrices + "\n";
+      averagePrice += productPrices;
+    }
+    sc.close();
+
+    if (finalOutput.contains("Peas")) {
+      total = averagePrice / itemNumber;
+      System.out.println("\n\n************\n" + finalOutput + "--------------\n" + "Average Price:" + total);
+    } else {
+      System.out.println("no average output");
+    }
+
+  }
+
+}
